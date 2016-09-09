2748f7383b984bb742c3729181db55cfd91a8b8b
敬超 <jing@turingca.com>
2016-09-09 12:08:46 +0800

    Create dot.js

---
+++ frontend/javascript/dot.js
@@ -0,0 +1,12 @@
+前言
+-----
+
+[官网](http://olado.github.io/doT/index.html)
+
+[Github](https://github.com/olado/doT)
+
+[中国爱好者](http://dotjs.cn/)
+
+The fastest + concise javascript template engine for Node.js and browsers.
+适用于Nodejs和浏览器的最快速简洁的JavaScript模板引擎
+
